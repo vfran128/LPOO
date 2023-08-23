7a3d984bd09086cb7d235aab609114a6bcc2f0b9
Franciso Vallejos <102253963+vfran128@users.noreply.github.com>
2023-08-23 08:52:27 -0300

    Update 16-8.java

--- 16-8.java
+++ 16-8.java
@@ -2,7 +2,7 @@
 public class ClasePrimaria {
 	public static void main(String[] args) {
 		//itera hasta 100
-		for (int i = 0; i <= 10; i++) {
+		for (int i = 0; i <= 100; i++) {
 			//calcula su resto al dividirse por 2
 		    int resto = i % 2;
 		    //si el resto es 0, es par, por ende lo imprime
