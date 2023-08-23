8058e70fe2d3fe0a23fe92a2cd04b2953e10aabe
Franciso Vallejos <102253963+vfran128@users.noreply.github.com>
2023-08-23 09:52:00 -0300

    Update 23-08.java

--- 23-08.java
+++ 23-08.java
@@ -11,5 +11,26 @@ public class ClasePrimaria {
 	return;
 	}
 }
-	        
+//PUNTO DOS
+import java.util.Scanner;
+
+public class ClasePrimaria {
+	public static void main(String[] args) {
+		int count = 0;
+		System.out.println("Ingrese su palabra");
+		Scanner scanner = new Scanner(System.in);
+		String texto = scanner.next().toLowerCase();
+		for (int i = 0; i < texto.length(); i++) {
+			char caracter = texto.charAt(i);
+			if(caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u') {
+				count ++;
+				}
+			}
+		
+	System.out.println("Hay "+count+" vocales");
+	scanner.close();
+	return;
+	}
+ }
+	
 	
