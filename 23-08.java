f3c9afd22675f7877737081c8a774199912ce7be
Franciso Vallejos <102253963+vfran128@users.noreply.github.com>
2023-08-23 09:42:32 -0300

    Create 23-08.java

---
+++ 23-08.java
@@ -0,0 +1,15 @@
+//PUNTO UNO
+import java.util.Scanner;
+
+public class ClasePrimaria {
+	public static void main(String[] args) {
+	System.out.println("Ingrese su nombre");
+	Scanner scanner = new Scanner(System.in);
+	String nombre = scanner.next();
+	System.out.println("Nombre = "+ nombre);
+	scanner.close();
+	return;
+	}
+}
+	        
+	
